a998153fd81e13a1d8c3a22dad161889040566a9
yung-wolf <edmundbansah@gmail.com>
2023-02-17 20:59:08 +0000

    The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	/* int ans holds the last digit of number*/
+	/* run an if else if loop to print the correct output */
+	int ans;
+
+	ans = n % 10;
+
+	if (ans > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, ans);
+	} else if (ans < 6 && ans != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, ans);
+	} else if (ans == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, ans);
+	}
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,5 +3,8 @@
 
 ```bash
 # This program will assign a random number to the variable n each time it is executed. Complete the source code in order# to print whether the number stored in the variable n is positive or negative.
-0-positive_or_negative
+0-positive_or_negative.c
+
+# Prints the last digit of given number
+1-last_digit.c
 ```
