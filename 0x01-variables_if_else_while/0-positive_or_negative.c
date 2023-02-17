246ab604ca5bcb297202b5e161aa24446ae52e13
yung-wolf <edmundbansah@gmail.com>
2023-02-17 19:50:34 +0000

    Positive over Negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* main - Entry point*/
+/* more headers goes there */
+/*Return: 0 if successful*/
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* my code goes there */
+	/* If confirms if number entered is positive ,negative, or zero*/
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	} else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,7 @@
+
+# Project Description
+
+```bash
+# This program will assign a random number to the variable n each time it is executed. Complete the source code in order# to print whether the number stored in the variable n is positive or negative.
+0-positive_or_negative
+```
