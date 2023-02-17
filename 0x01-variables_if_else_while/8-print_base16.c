4a56148bfea4d1c6aef16c01e2989c9b3a5e91d9
yung-wolf <edmundbansah@gmail.com>
2023-02-17 23:55:44 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,8 +3,7 @@
 /**
  *main - prints hexadecimal base 0123456789abcdef, using putchar
  *Return: Always 0 (Success)
- */ 
-
+ */
 int main(void)
 {
 	int n = '0';
@@ -15,13 +14,11 @@ int main(void)
 		putchar(n);
 		n++;
 	}
-
 	while (a_to_f <= 'f') /*print a-f to finish hexbase*/
 	{
 		putchar(a_to_f);
 		a_to_f++;
 	}
-
 	putchar('\n');
 
 	return (0);
