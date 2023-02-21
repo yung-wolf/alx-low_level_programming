7f40a65189bcd9f1326597c5f0c895099106b6b2
yung-wolf <edmundbansah@gmail.com>
2023-02-21 21:52:19 +0000

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_sign - Returns 1,0,-1 for positve num, zero, and negative num respectively w/ sign
+ * print_sign - Returns 1,0,-1 for positve num, zero,
+ * and negative num respectively w/ sign
  * @n: type int character to test
  * Return: 0,1,-1
  */
@@ -17,11 +18,11 @@ int print_sign(int n)
 	{
 		_putchar('0');
 		return (0);
-	} 
-	else (n < 0)
+	}
+	else
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 
 }
