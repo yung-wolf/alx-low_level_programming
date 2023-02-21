b0034b3314e424e35ff1397ed42ba3976357b9bf
yung-wolf <edmundbansah@gmail.com>
2023-02-21 23:54:28 +0000

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,19 @@
 
 int print_last_digit(int x)
 {
+	int a;
+
 	if (x < 0)
 	{
-		x *= -1;	
-		_putchar('0' + (x % 10));
+		int a = -(x % 10);
+
+		_putchar(a + '0');
 	}
 	else
 	{
 		_putchar('0' + (x % 10));
+		int a = (x % 10);
 	}
 
-	return (x % 10);
+	return (a);
 }
