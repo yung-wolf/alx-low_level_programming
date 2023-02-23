513bddcc8de32191a97a902b49b56ceaf86a17c7
yung-wolf <edmundbansah@gmail.com>
2023-02-23 11:02:34 +0000

    return largest num

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,9 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+/* Returns positive, negative, or neutral num*/
+
+int largest_number(int a, int b, int c);
+/* returns largest num out of three nums*/
 
 #endif /* MAIN_H */
