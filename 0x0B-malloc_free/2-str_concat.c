18cb7b02141e609811705a29ef37801fda4a50a7
yung-wolf <edmundbansah@gmail.com>
2023-03-14 21:28:34 +0000

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	while (*(s1 + i))
 		len1++, i++;
-	while (*(s2 = j))
+	while (*(s2 + j))
 		len2++, j++;
 	len2++;
 	concat = malloc((len1 + len2) * sizeof(char));
