42d679f1b54630b3b6fe321de380fdfb63e51ba7
yung-wolf <edmundbansah@gmail.com>
2023-03-14 20:20:01 +0000

    Initial Commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - create array of chars and initialize with specific char
+ * @size: size of array
+ * @c: fill array values with this char
+ * Return: pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+
+	if (arr == NULL)
+		return (NULL);
+
+	while (i < (int)size)
+	{
+		*(arr + i) = c;
+		i++;
+	}
+	*(arr + i) = '\0';
+
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+
+# Dynamic Memory Allocation (Malloc)
+
+0. Function that creates an array of chars, and init it with specific char.
