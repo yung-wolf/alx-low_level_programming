58db3f19974a9b771991ad158b4fa1687c105b67
Eli <62481766+yung-wolf@users.noreply.github.com>
2023-03-20 19:15:56 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -38,11 +38,10 @@ char *strcpy(char *dest, char *src)
  * @owner: member
  * Return: initialized instance of struct dog
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog1;
-	char *copy_of_name;
-	char *copy_of_owner;
 
 	dog1 = malloc(sizeof(dog_t)); /* validate if dog1 initiated correctly */
 	if (dog1 == NULL)
@@ -52,32 +51,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	/* make copies of struct members and validate, else free on error */
 	/* set values of struct members to copies of arguments or set to NULL */
-	if (name != NULL)
-	{
-		copy_of_name = malloc(len(name) + 1);
-		if (copy_of_name == NULL)
-		{
-			free(dog1);
-			return (NULL);
-		}
-		dog1->name = strcpy(copy_of_name, name);
-	}
-	else
-		dog1->name = NULL;
-
-	if (owner != NULL)
-	{
-		copy_of_owner = malloc(len(owner) + 1);
-		if (copy_of_owner == NULL)
-		{
-			free(copy_of_name);
-			free(dog1);
-			return (NULL);
-		}
-		dog1->owner = strcpy(copy_of_owner, owner);
-	}
-	else
-		dog1->owner = NULL;
-
+	
 	return (dog1);
 }
