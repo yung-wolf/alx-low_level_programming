1a5f064a50dcfeed8cc69d3e36218ad300081ea5
yung-wolf <edmundbansah@gmail.com>
2023-03-20 12:38:59 +0000

    Function to init type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * init_dog - creates an instance of a dog
+ * @d: pointer to dog instance
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -2,3 +2,4 @@
 # Struct and Typedef Project
 
 0. Poppy. Define struct dog with elements; name, age, owner.
+1. Create a function that initialize the var type 'struct dog'.
