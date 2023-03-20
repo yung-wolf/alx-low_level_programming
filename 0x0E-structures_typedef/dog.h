23468a896aa2c451d56e3bedfcec46827316caea
yung-wolf <edmundbansah@gmail.com>
2023-03-20 12:22:37 +0000

    fixed betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struck dog - models a dog
+ * struct dog - models a dog
  * @name: first member
  * @age: second member
  * @owner: Third member
  *
  * Description: Holds basic info about a dog
  */
-
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
