2c09f1df021e7e5bdb2e30ae3453c416f0d92af9
yung-wolf <edmundbansah@gmail.com>
2023-03-20 14:02:54 +0000

    fixed if statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,15 +10,17 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", d->name);
+
 	if (d->name == NULL)
 		d->name = "(nil)";
+	printf("Name: %s\n", d->name);
+
 	if (d->age == NULL)
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %d\n", d->age);
+
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-
 	printf("Owner: %s\n", d->owner);
 }
