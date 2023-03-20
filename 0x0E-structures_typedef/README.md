aa14ecbb264786915e0e2798ffe3867f4e082b39
yung-wolf <edmundbansah@gmail.com>
2023-03-20 12:16:45 +0000

    Initial Commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+
+# Struct and Typedef Project
+
+0. Poppy. Define struct dog with elements; name, age, owner.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struck dog - models a dog
+ * @name: first member
+ * @age: second member
+ * @owner: Third member
+ *
+ * Description: Holds basic info about a dog
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
