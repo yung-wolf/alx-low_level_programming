608722ff54757e65f9e5ae01b1a7bed5817c1361
yung-wolf <edmundbansah@gmail.com>
2023-03-20 14:41:36 +0000

    Creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,12 @@
-#ifndef DOG_H
-#define DOG_H
-
+#include "dog.h"
 /**
- * struct dog - models a dog
+ * dog_t new_dog - models a dog
  * @name: first member
  * @age: second member
  * @owner: Third member
  *
  * Description: Holds basic info about a dog
  */
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -28,7 +20,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->age = age;
 	dog->owner = owner;
 }
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-#endif
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -4,3 +4,4 @@
 0. Poppy. Define struct dog with elements; name, age, owner.
 1. Create a function that initialize the var type 'struct dog'.
 2. A function to print details of struct dog.
+3. Function to create a new dog.
