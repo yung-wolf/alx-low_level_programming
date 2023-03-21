51b890a126671b9ac6d546fb1e001538faa4d742
yung-wolf <edmundbansah@gmail.com>
2023-03-21 09:35:51 +0000

    Removed trialing whitespaces from file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,10 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog1 = malloc(sizeof(dog_t)); /* validate if dog1 initiated correctly */
 	if (dog1 == NULL)
 		return (NULL);
-	
+
 	dog1->name = name;
 	dog1->age = age;
 	dog1->owner = owner;
-	
+
 	return (dog1);
 }
