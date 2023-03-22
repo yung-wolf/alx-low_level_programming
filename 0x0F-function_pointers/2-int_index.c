d235bfe5c8585de3ad0af92b0d514cd0c150473f
yung-wolf <edmundbansah@gmail.com>
2023-03-22 13:32:31 +0000

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * int_index - function that searches for an int
+ * @array: array arg
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: index of first occurance of matched value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, x;
+
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			x = cmp(array[i]);
+			if (x != 0)
+				return (i);
+		}
+	}
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -3,3 +3,4 @@
 
 0. A function that prints a name.
 1. A function that executes a function given as a parameter on each element in an array.
+2. A function that searches for an int.
