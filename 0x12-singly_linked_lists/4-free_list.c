f79947d5ada16df052e7ca8fd92f4c03645d4105
yung-wolf <edmundbansah@gmail.com>
2023-03-31 00:09:42 +0000

    removed alternate way of solving task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -14,19 +14,3 @@ void free_list(list_t *head)
 		head = head->next;
 	}
 }
-/**
- * another way using temp pointer
- *
- *      list_t *ptr;
- *
- *	if (head == NULL) // account for no linked list
- *		return;
- *
- *	while (head != NULL) // have ptr keep track of head node and free
- *	{
- *		ptr = head;
- *		head = head->next; // move to next node while ptr frees prior
- *		free(ptr->str); // free malloced strings
- *		free(ptr);
- *	}
- */
