459fe8a50bdcb37b10c77558c35b3e57fe47265d
yung-wolf <edmundbansah@gmail.com>
2023-04-05 01:31:48 +0000

    hot fix file 3 & 9

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,9 +10,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *ptr;
 
-	if (!(n))
-		return (NULL);
-
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
