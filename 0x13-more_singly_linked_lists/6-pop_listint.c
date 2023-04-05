cc650175a4fa41503888a96000afb433fabef9b3
yung-wolf <edmundbansah@gmail.com>
2023-04-05 01:59:02 +0000

    Create 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of linked list
+ * @head: address of linked list
+ * Return: head node's data(n)
+ */
+int pop_listint(listint_t **head)
+{
+	if (*head == NULL)
+		return (0);
+
+	listint_t *tmp = *head;
+
+	head = head->link;
+
+	return (tmp->n);
+}
