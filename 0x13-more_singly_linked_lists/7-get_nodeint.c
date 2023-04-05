ddaabd6ada524616c959768fd315b67b6d8844ea
yung-wolf <edmundbansah@gmail.com>
2023-04-05 03:03:09 +0000

    fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,26 +1,6 @@
 #include "lists.h"
 
 /**
- * list_len - gets number of elements in a linked list
- * @h: linked list
- * Return: number of elements
- */
-size_t list_len(const listint_t *h)
-{
-	size_t count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-/**
  * get_nodeint_at_index - returns the nth node of linked list
  * @head: linked list
  * @index: index or n
@@ -29,18 +9,18 @@ size_t list_len(const listint_t *h)
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-	size_t limit = listint_len(head);
 
 	if (head == NULL)
 		return (NULL);
-	if ((size_t)index > limit)
-		return (NULL);
 
-	while (count < index)
+	while (count < index && head != NULL)
 	{
 		count++;
 		head = head->next;
 	}
+
+	if (count != index)
+		return (NULL);
 
 	return (head);
 }
