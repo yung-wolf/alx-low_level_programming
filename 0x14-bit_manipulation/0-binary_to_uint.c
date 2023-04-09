3094f5dd200b053a3d4cb9d474ceed0ffddb13b8
yung-wolf <edmundbansah@gmail.com>
2023-04-09 13:26:56 +0000

    fix 9

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,7 @@
  * @b: holds a char of binary nums
  * Return: converted num, 0 if *b holds one or more char not 0 or 1
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int decimal = 0;
 	int i = 0, j = 0, len = 0;
