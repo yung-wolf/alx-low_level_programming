ceb3af6d7bc8ab3ecc9090877bc217fb4cc1a078
yung-wolf <edmundbansah@gmail.com>
2023-04-09 02:09:25 +0000

    fix 4

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,8 @@
  */
 unsigned int binary_to_unit(const char *b)
 {
-	int decimal = 0, i = 0, j = 0, len = 0;
+	unsigned int decimal = 0;
+	int i = 0, j = 0, len = 0;
 
 	if (b == NULL)
 		return (decimal);
