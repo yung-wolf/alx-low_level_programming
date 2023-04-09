2b43e059de0fc064a7d89f6b64149959a742d226
yung-wolf <edmundbansah@gmail.com>
2023-04-09 22:18:12 +0000

    Create 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - set value of a bit to 1 at given index
+ * @n: number
+ * @index: index
+ * Return: 1 if successful, -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int max;
+	unsigned int var;
+
+	max = (sizeof(unsigned long int) * 8);
+	if (index > max)
+		return (-1);
+
+	var = ~(1 << index);
+	*n = *n & var;
+
+	return (1);
+}
