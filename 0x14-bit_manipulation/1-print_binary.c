1639e751b241ebf29a5f0a96d5059a7a1ae655a6
yung-wolf <edmundbansah@gmail.com>
2023-04-09 19:37:33 +0000

    fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,14 +8,14 @@ void print_binary(unsigned long int n)
 {
 	int result, bi = 0;
 
-	if (num < 1)
+	if (n < 1)
 		;
 	else
 	{
-		result = num >> 1;
-		bi = num - (result * 2);
-		num = result;
-		print_binary(num);
+		result = n >> 1;
+		bi = n - (result * 2);
+		n = result;
+		print_binary(n);
 		_putchar(bi + '0');
 	}
 }
