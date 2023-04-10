fdd495acf9a24705bc13e8a4650872b815044f6c
yung-wolf <edmundbansah@gmail.com>
2023-04-10 14:41:42 +0000

    Modified file 4 and created file 5

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - set value of a bit to 1 at given index
+ * clear_bit - set value of a bit to 1 at given index
  * @n: number
  * @index: index
  * Return: 1 if successful, -1 if error
