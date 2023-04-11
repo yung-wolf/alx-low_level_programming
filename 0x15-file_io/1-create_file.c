6824489a2e8b990c329eb0a98f8da3eeed55539c
yung-wolf <edmundbansah@gmail.com>
2023-04-11 02:55:17 +0000

    Initial commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: words to write to file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0, i = 0;
+	size_t num_write;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		;
+	else
+	{
+		while (text_content[i] != '\0')
+		{
+			len++;
+			i++;
+		}
+
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	num_write = write(fd, text_content, len);
+	if (num_write == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	return (1);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <fcntl.h>
+
+/* Function prototypes*/
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+#endif
