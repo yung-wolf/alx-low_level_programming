c93b49fc0fb405727fd35f49f323d053a155038a
elib <edmundbansah@gmail.com>
2023-07-07 03:00:49 -0700

    fixed 0-binary...c and main.f files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] == 0 || b[i] == 1)
+		if (b[i] == '0' || b[i] == '1')
 			answer = answer * 2 + (b[i] - '0');
 		else
 			return (0);
