26baa245180ef5a9dde52e8012647fb363f3b7d8
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-18 08:26:54 +0000

    fixed betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,16 +5,17 @@
  * @h: head of node
  * Return: number of elements in node
  */
-size_t print_dlistint(const dlistint_t *h){
-	/* ptr to hold head var*/
-	size_t numOfNodes = 0;
+size_t print_dlistint(const dlistint_t *h)
+{
+        /* ptr to hold head var*/
+        size_t numOfNodes = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		numOfNodes++;
-	}
+        while (h != NULL)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+                numOfNodes++;
+        }
 
-	return (numOfNodes);
+        return (numOfNodes);
 }
