de8f8e4e0717857c74d33461475cbc25124fbf40
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-18 12:30:10 +0000

    handle index out of range condition II

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		numOfNodes++;
 	}
 
-	if (idx >= numOfNodes)
+	if (idx >= numOfNodes || idx < 0)
 		return (NULL);
 
 	while (idx - 1 != idxOfNodes)
