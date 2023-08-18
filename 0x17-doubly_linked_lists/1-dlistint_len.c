8597fa91d9d24cbb3bc72c9e1c43f316ca974b2a
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-18 08:39:43 +0000

    fixed betty style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,34 +1,18 @@
-#include <stdio.h>
-
-/**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * 
- */
-typedef struct dlistint_s
-{
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-} dlistint_t;
+#include "lists.h"
 
 /**
  * dlistint_len - get len of nodes
  * @h: head of node
  * Return: number of elements in node
  */
-size_t dlistint_len(const dlistint_t *h){
-	/* ptr to hold head var*/
+size_t dlistint_len(const dlistint_t *h)
+{
 	size_t numOfNodes = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		numOfNodes ++;
+		numOfNodes++;
 	}
 
 	return (numOfNodes);
