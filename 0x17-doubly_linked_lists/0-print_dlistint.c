94f925954e162dabfd8758064c48646c21d26732
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-08-18 03:14:38 +0000

    fix 1 for 0-print...

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,4 @@
-#include <stdio.h>
-
-/**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * 
- */
-typedef struct dlistint_s
-{
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-} dlistint_t;
+#include "list.h"
 
 /**
  * print_dlistint - prints all elements
