54df5f80cc2c3249ecba5f6233295dfd4be8a2c0
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-09-09 18:39:54 +0000

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - printd a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i, count = 0;
+
+	if (ht)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				if (count)
+					printf(", ");
+				printf("'%s': '%s'", node-> key, node->value);
+				count = 1;
+				node = node->next;
+			}
+		}
+		printf("}\n");
+	}
+	else
+		printf("{}\n");
+}
