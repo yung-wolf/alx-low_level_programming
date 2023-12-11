19ee60356a41a67f81c6dd4c23921f4d816a08e5
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-12-11 16:06:06 +0000

    Initial commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Implement linear search alogrithm to find value
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: target value
+ * Return: the index of value in array
+ */ 
+
+int linear_search(int *array, size_t size, int value)
+{
+	/* declare index and counter i . check == 1 if value is found*/
+	int i, check = 0;
+
+	for (i = 0; i < (int) size; i++)
+	{
+		if (check == 0)
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+		{
+			check = 1;
+			return (i);
+		}
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,13 @@
+# 0x1E-search_algorithms
+This project includes tasks that explore search algorithms and their space complexities
+
+# Requirements
+- Compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+- Checked using betty-style.pl and betty-doc.pl
+- No more than 5 functions per file
+- Only allowed to use the printf function of the standard library
+- The prototypes of all your functions should be included in your header file called search_algos.h
+- All your header files should be include guarded
+
+# Author
+ELI Bansah (yung-wolf)
