a50b2c71c5aa2e2c6b5f746481df219d1f68eeff
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-12-12 22:02:50 +0000

    Fix IV

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,18 +1,46 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in a sorted array of integers using
- * the Binary search algorithm
+ * minm - gets the minimum value between two numbers
+ * @a: num1
+ * @b: num2
+ * Return: the minimum value
+ */
+int minm(int a, int b)
+{
+	int x, y;
+
+	if (a < 0)
+		x = a * -1;
+	else
+		x = a;
+
+	if (b < 0)
+		y = b * -1;
+	else
+		y = b;
+
+	if ((y - x) > 0)
+		return (a);
+
+	return (b);
+}
+
+/**
+ * cust_binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm with a lil twist
  * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
  * @value: value to search for
+ * @bound_beg: begining bound value of exponential search
+ * @bound_end: ending bound value of exponential search
  * Return: index of value in array on success, -1 on failure
  */
 
-int binary_search(int *array, size_t size, int value)
+int cust_binary_search(int *array,
+int value, size_t bound_beg, size_t bound_end)
 {
-	int left = 0, m, i;
-	int right = (int) size - 1;
+	int left = bound_beg, m, i;
+	int right = (int) bound_end - 1;
 
 	if (array == NULL)
 		return (-1);
@@ -50,8 +78,7 @@ int binary_search(int *array, size_t size, int value)
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t bound, new_b, new_size;
-	int i;
+	size_t bound;
 
 	if (array == NULL)
 		return (-1);
@@ -59,20 +86,12 @@ int exponential_search(int *array, size_t size, int value)
 	bound = 1;
 	while (bound < size && array[bound] < value)
 	{
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
 		bound *= 2;
 	}
 
-	/* create new array with low bound as first element */
-	new_b = bound / 2, i = 0;
-	new_size = size - new_b;
-	int new_arr[new_size];
+	printf("Value found between indexes [%lu] and [%d]\n",
+	bound / 2, minm(bound, size - 1));
 
-	while (new_b <= size)
-	{
-		new_arr[i] = array[new_b];
-		i++;
-		new_b++;
-	}
-
-	return (binary_search(new_arr, new_size, value));
+	return (cust_binary_search(array, value, bound / 2, minm(bound + 1, size)));
 }
